6911635795fa7d12377cf06cf33c4900c8457778
michael.krog <michael.krog@7767cefd-0c50-1314-e531-0c53e31ed622>
2011-07-25 14:41:05 +0000

    First import

---
+++ trunk/print-facade-core/src/main/java/dk/apaq/printing/core/PrinterCapabilities.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dk.apaq.printing.core;
+
+/**
+ *
+ * @author michael
+ */
+public class PrinterCapabilities {
+
+
+}
---
+++ trunk/print-facade-core/src/main/java/dk/apaq/printing/core/PrinterManager.java
@@ -0,0 +1,20 @@
+package dk.apaq.printing.core;
+
+/**
+ *
+ * @author michael
+ */
+public class PrinterManager {
+
+    public void addPlugin(PrinterManagerPlugin plugin) {
+
+    }
+
+    public void removePlugin(PrinterManagerPlugin plugin) {
+
+    }
+
+    public void print(PrinterJob job) {
+        
+    }
+}
