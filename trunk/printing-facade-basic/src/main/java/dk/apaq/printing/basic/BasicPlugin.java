eca3dd661bc742302332dfd6ea189d4a0f6d98f5
michael.krog <michael.krog@7767cefd-0c50-1314-e531-0c53e31ed622>
2011-08-01 16:27:03 +0000

    Added initial support for basic printing.

---
+++ trunk/printing-facade-basic/src/main/java/dk/apaq/printing/basic/BasicPlugin.java
@@ -0,0 +1,57 @@
+package dk.apaq.printing.basic;
+
+import dk.apaq.printing.core.AbstractPrinterManagerPlugin;
+import dk.apaq.printing.core.Margin;
+import dk.apaq.printing.core.Paper;
+import dk.apaq.printing.core.Printer;
+import dk.apaq.printing.core.PrinterJob;
+import dk.apaq.printing.core.PrinterListChangeListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.attribute.Attribute;
+import javax.print.attribute.standard.Chromaticity;
+import javax.print.attribute.standard.ColorSupported;
+
+/**
+ *
+ * @author michael
+ */
+public class BasicPlugin extends AbstractPrinterManagerPlugin {
+
+    private final Printer defaultPrinter;;
+    private List<Printer> printers = new ArrayList<Printer>();
+
+    public BasicPlugin() {
+        PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
+        for(PrintService ps : services) {
+            printers.add(new BasicPrinter(ps));
+        }
+
+        PrintService defaultPrintService = PrintServiceLookup.lookupDefaultPrintService();
+        if(defaultPrintService!=null) {
+            defaultPrinter = new BasicPrinter(defaultPrintService);
+        } else {
+            defaultPrinter = null;
+        }
+    }
+
+    public Printer getDefaultPrinter() {
+        return defaultPrinter;
+    }
+
+    public List<Printer> getPrinters() {
+        return Collections.unmodifiableList(printers);
+    }
+
+    public void print(PrinterJob job) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    
+
+}
---
+++ trunk/printing-facade-basic/src/test/java/dk/apaq/printing/basic/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dk.apaq.printing.basic;
+
+import dk.apaq.printing.core.Paper;
+import dk.apaq.printing.core.Printer;
+
+/**
+ *
+ * @author michael
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        BasicPlugin basicPlugin = new BasicPlugin();
+
+        for(Printer printer : basicPlugin.getPrinters()) {
+            System.out.println(printer.getName() + "***************");
+            for(Paper paper : printer.getSupportedPapers()) {
+                System.out.println("Paper: " + paper.getWidth() + "x" + paper.getHeight());
+            }
+        }
+    }
+}
