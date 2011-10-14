33a68a1501f97483ac1f3c558aaffc6fffeb334c
michael.krog <michael.krog@7767cefd-0c50-1314-e531-0c53e31ed622>
2011-10-14 14:39:50 +0000

    More work on Google Cloud Print plugin.

--- trunk/printing-facade-googlecloud/src/main/java/dk/apaq/printfacade/googlecloud/GoogleCloudPrintPlugin.java
+++ trunk/printing-facade-googlecloud/src/main/java/dk/apaq/printfacade/googlecloud/GoogleCloudPrintPlugin.java
@@ -1,9 +1,19 @@
 package dk.apaq.printfacade.googlecloud;
 
+import com.google.gson.Gson;
 import dk.apaq.printing.core.AbstractPrinterManagerPlugin;
 import dk.apaq.printing.core.Printer;
+import dk.apaq.printing.core.PrinterException;
 import dk.apaq.printing.core.PrinterJob;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -11,19 +21,41 @@ import java.util.List;
  */
 public class GoogleCloudPrintPlugin extends AbstractPrinterManagerPlugin {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GoogleCloudPrintPlugin.class);
+    private static final String tokenUrl = "https://accounts.google.com/o/oauth2/token";
     /*
      * http://code.google.com/p/google-api-java-client/wiki/OAuth2Draft10#Sample_Program
      * http://code.google.com/apis/cloudprint/docs/appInterfaces.html
      * http://code.google.com/apis/accounts/docs/OAuth2.html#SS
+     * 
+     * code = 4/s0xkDRwfdiZR9rthiZdDCqKvekjP
+     * clientid = 700939733854.apps.googleusercontent.com
+     * clientSecret = yygJ1QE3yRsNieDoZKOKBUgl
      */
+    private final String clientId;
+    private final String clientSecret;
     private final String authorizationCode;
+    private AccessTokenInfo accessTokenInfo;
+    private Date lastTokenUpdate = null;
+    private Gson gson = new Gson();
 
-    public GoogleCloudPrintPlugin(String authorizationCode) {
+    private class AccessTokenInfo {
+
+        private String accessToken;
+        private int expiresIn;
+        private String tokenType;
+        private String refresh_token;
+    }
+
+    public GoogleCloudPrintPlugin(String clientId, String clientSecret, String authorizationCode) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
         this.authorizationCode = authorizationCode;
     }
-    
+
     public Printer getDefaultPrinter() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String token = getAccessToken();
+        return null;
     }
 
     public List<Printer> getPrinters() {
@@ -33,8 +65,56 @@ public class GoogleCloudPrintPlugin extends AbstractPrinterManagerPlugin {
     public void print(PrinterJob job) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private String getAccessToken() {
-        return null;
+
+    private String getAccessToken() throws PrinterException {
+        
+        if(!isTokenExpired()) {
+            return accessTokenInfo.accessToken;
+        }
+        
+        HttpURLConnection con = null;
+        try {
+            URL url = new URL(tokenUrl);
+            con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            //con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+            writer.write("code=" + authorizationCode);
+            writer.write("&client_id=" + clientId);
+            writer.write("&client_secret=" + clientSecret);
+            
+            if(accessTokenInfo == null) {
+                writer.write("&grant_type=authorization_code");
+                writer.write("&redirect_uri=http://localhost/oauth2callback");
+            } else {
+                writer.write("&grant_type=refresh_token");
+                writer.write("&refresh_token="+accessTokenInfo.accessToken);
+            }
+            writer.close();
+
+            accessTokenInfo = gson.fromJson(new InputStreamReader(con.getInputStream()), AccessTokenInfo.class);
+
+            LOG.info("Got new token from Google: " + accessTokenInfo.accessToken);
+            return accessTokenInfo.accessToken;
+        } catch (IOException ex) {
+            String message = "Could not retrieve access token from Google. ";
+            if (con instanceof HttpURLConnection) {
+                try {
+                    message += " " + con.getResponseMessage();
+                } catch (IOException ex1) {
+                }
+            }
+            throw new PrinterException(message, ex);
+        }
+
+    }
+
+    private boolean isTokenExpired() {
+        if (lastTokenUpdate == null || (System.currentTimeMillis() - lastTokenUpdate.getTime()) >= (accessTokenInfo.expiresIn * 1000)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ trunk/printing-facade-googlecloud/src/test/java/dk/apaq/printfacade/googlecloud/GoogleCloudPrintPluginTest.java
@@ -0,0 +1,78 @@
+package dk.apaq.printfacade.googlecloud;
+
+import dk.apaq.printing.core.Printer;
+import dk.apaq.printing.core.PrinterJob;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author krog
+ */
+public class GoogleCloudPrintPluginTest {
+
+    public GoogleCloudPrintPluginTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        String code = "4/s0xkDRwfdiZR9rthiZdDCqKvekjP";
+        String clientid = "700939733854.apps.googleusercontent.com";
+        String clientSecret = "TwM1ADz1REuRfd5muU89Rejv";
+        cloudPrintPlugin = new GoogleCloudPrintPlugin(clientid, clientSecret, code);
+    }
+
+    @After
+    public void tearDown() {
+    }
+    private GoogleCloudPrintPlugin cloudPrintPlugin;
+
+    /**
+     * Test of getDefaultPrinter method, of class GoogleCloudPrintPlugin.
+     */
+    @Test
+    public void testGetDefaultPrinter() {
+        System.out.println("getDefaultPrinter");
+        Printer result = cloudPrintPlugin.getDefaultPrinter();
+        //assertEquals(expResult, result);
+    }
+    /**
+     * Test of getPrinters method, of class GoogleCloudPrintPlugin.
+     */
+    /*@Test
+    public void testGetPrinters() {
+    System.out.println("getPrinters");
+    GoogleCloudPrintPlugin instance = null;
+    List expResult = null;
+    List result = instance.getPrinters();
+    assertEquals(expResult, result);
+    // TODO review the generated test code and remove the default call to fail.
+    fail("The test case is a prototype.");
+    }
+     */
+    /**
+     * Test of print method, of class GoogleCloudPrintPlugin.
+     */
+    /*@Test
+    public void testPrint() {
+    System.out.println("print");
+    PrinterJob job = null;
+    GoogleCloudPrintPlugin instance = null;
+    instance.print(job);
+    // TODO review the generated test code and remove the default call to fail.
+    fail("The test case is a prototype.");
+    }*/
+}
