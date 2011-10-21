5e7e4aaa42272433c4dc1f43eebc0aef70d0ab01
michael.krog <michael.krog@7767cefd-0c50-1314-e531-0c53e31ed622>
2011-10-21 12:03:45 +0000

    Added image module and refactored stuff a bit.

---
+++ printing-facade-remoteclient/src/main/java/dk/apaq/printing/remoteclient/RemoteClientPlugin.java
@@ -0,0 +1,83 @@
+package dk.apaq.printing.remoteclient;
+
+import dk.apaq.printing.core.AbstractPrinterManagerPlugin;
+import dk.apaq.printing.core.Paper;
+import dk.apaq.printing.core.Printer;
+import dk.apaq.printing.core.PrinterException;
+import dk.apaq.printing.core.PrinterJob;
+import dk.apaq.printing.core.util.ImageUtil;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author michaelzachariassenkrog
+ */
+public class RemoteClientPlugin extends AbstractPrinterManagerPlugin {
+
+    private final RemoteClientPluginTransporter transporter;
+    private final List<Printer> printers = new ArrayList<Printer>();
+    private String defaultId;
+    private static final int RESOLUTION = 300;
+
+    public RemoteClientPlugin(RemoteClientPluginTransporter transporter) {
+        this.transporter = transporter;
+    }
+
+    @Override
+    public Printer getDefaultPrinter() {
+        if (defaultId == null) {
+            return null;
+        }
+
+        for (Printer p : printers) {
+            if (p.getId().equals(defaultId)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Printer> getPrinters() {
+        return Collections.unmodifiableList(printers);
+    }
+
+    @Override
+    public void print(PrinterJob job) {
+        transporter.transportPrinterJob(buildZip(job));
+    }
+
+    public void setPrinters(List<RemoteClientPrinter> clientPrinters) {
+        this.printers.clear();
+        this.printers.addAll(clientPrinters);
+        fireChangeEvent();
+    }
+
+    public void setDefaultPrinterId(String id) {
+        this.defaultId = id;
+    }
+
+    private byte[] buildZip(PrinterJob job) {
+        try {
+            return ImageUtil.buildImage(job, RESOLUTION);
+        } catch (IOException ex) {
+            throw new PrinterException("Unable to print.", ex);
+        }
+
+    }
+
+    
+}
